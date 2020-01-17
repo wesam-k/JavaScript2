e437b65e14034e0f7df299073bce2458736ff23d
wesam <user.name>
2020-01-17 17:39:05 +0100

    update ex- 5

--- Week3/js-exercises-w3/5-The lottery machine.js
+++ Week3/js-exercises-w3/5-The lottery machine.js
@@ -2,25 +2,34 @@
 
 function threeFive(startIndex, stopIndex, threeCallback, fiveCallback) {
     const numbers = [];
-       for (let i = startIndex ; i <= stopIndex; i++){
-            numbers.push(i);
-           }
-           
-       console.log(numbers);
-      threeCallback(numbers);
-      fiveCallback(numbers);
+    for (let i = startIndex ; i <= stopIndex; i++){
+      numbers.push(i); 
+    }
+    
+ 
+    numbers.forEach(num => {
+      if(num % 3 === 0){
+        threeCallback(num)
+      }
+      if(num % 5 === 0){
+        fiveCallback(num);
+      }
+   
+    });
+   
 
 }
 
-function sayThree(newArray){
-let  numberThree = newArray.filter(num => num % 3 === 0)
-  console.log(` ${numberThree} are divisible by 3 `)
+function sayThree(number){
+  console.log(` ${number} is divisible by 3 `)
+}   
+function sayFive(number){
+  console.log(` ${number} is divisible by 5`)
 }   
 
-function sayFive(newArray){
-let  numberFive = newArray.filter(num => num % 5 === 0)
- console.log(` ${numberFive} are divisible by 5 `)
-}   
+
         
-threeFive(10,15,sayThree, sayFive)
+ threeFive(10,15,sayThree, sayFive)
+
+
     
